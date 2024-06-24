80b688dc089d43a196fff6f22e6e8b55cd6ce00a
Erica Blom <97736126+Erica-Blom@users.noreply.github.com>
2024-06-24 12:16:42 +0200

    Update teliaTvStreaming.js

--- script/teliaTvStreaming.js
+++ script/teliaTvStreaming.js
@@ -41,7 +41,6 @@ window.addEventListener('DOMContentLoaded', () => {
   chatbubbleStyle.textContent = `
     .bubble{position:absolute !important; bottom:105px;right:900px;display:none !important}
     .container{position:fixed !important; bottom:0px;right:0px }
-    .chat-wrapper{animation: 1s ease-out 0s 1 slideOutToRight; !important}
     .chat-wrapper.expanded{bottom:0px !important; animation: .25s ease forwards slideInFromRight; border-top-right-radius:0px !important; }
     .chat-wrapper{transform:none !important; transition:none !important}
     
